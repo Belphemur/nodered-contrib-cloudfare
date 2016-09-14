f0bf5de64c599cbd271ee9cd9b95a18604c8ee53
Antoine Aflalo <antoine.aflalo@zerospam.ca>
2016-09-14 12:58:47 -0400

    Frist commit

---
+++ cloudfare-get.js
@@ -0,0 +1,31 @@
+/**
+ * Created by aaflalo on 14/09/16.
+ */
+
+
+module.exports = function (RED) {
+
+    var CloudScrapper = require('cloudscraper');
+
+    function CloudFareGetNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        var url = this.url;
+        this.on('input', function (msg) {
+            url = msg.url ? msg.url : url;
+            var headers = {} || msg.headers;
+            CloudScrapper.get(url, function (error, response, body) {
+                if(error) {
+                    node.error(error);
+                    return;
+                }
+
+                msg.payload = body;
+                msg.response = response;
+                node.send(msg);
+            }, headers);
+        });
+    }
+
+    RED.nodes.registerType("cloudfare-get", CloudFareGetNode);
+};
